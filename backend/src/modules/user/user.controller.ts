4c5369d1a14593dfd77208892b4adf4c55437150
StarchenkoDmitry <StarchenkoDmitryPavlovich@gmail.com>
2025-12-11 17:06:41 +0300

    feat(backend): add me endpoint to UserController

--- backend/src/modules/user/user.controller.ts
+++ backend/src/modules/user/user.controller.ts
@@ -1,7 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
 import { UserService } from './user.service';
+import { AuthSessionGuard } from 'src/core/auth/auth.guard';
+import { User } from 'src/entities/user.entity';
+import { GetUser } from 'src/core/auth/auth.decorator';
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
+
+  @UseGuards(AuthSessionGuard)
+  @Get('me')
+  findOne(@GetUser() user: User) {
+    return user;
+  }
 }
