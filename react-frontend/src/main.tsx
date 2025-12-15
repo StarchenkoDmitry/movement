7fcede20dd03bbe0d15a9f0624c52227ec1c4589
StarchenkoDmitry <StarchenkoDmitryPavlovich@gmail.com>
2025-12-15 15:45:12 +0300

    feat(react-frontend): install and configure material ui tailwindcss, react-social-login-buttons

--- react-frontend/src/main.tsx
+++ react-frontend/src/main.tsx
@@ -1,10 +1,24 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
 
-createRoot(document.getElementById('root')!).render(
+import { StyledEngineProvider } from "@mui/material/styles";
+import GlobalStyles from "@mui/material/GlobalStyles";
+
+import CssBaseline from "@mui/material/CssBaseline";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <StyledEngineProvider enableCssLayer>
+      <GlobalStyles styles="@layer theme, base, mui, components, utilities;" />
+
+      <CssBaseline />
+      <App />
+    </StyledEngineProvider>
+  </StrictMode>
+);
