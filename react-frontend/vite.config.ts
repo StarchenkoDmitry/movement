a55a249c05bcc72ace9dc993b20c37a1fec5c13b
StarchenkoDmitry <StarchenkoDmitryPavlovich@gmail.com>
2025-12-16 19:21:05 +0300

    update(react-frontend): add path alias

--- react-frontend/vite.config.ts
+++ react-frontend/vite.config.ts
@@ -10,6 +10,7 @@ export default defineConfig({
     alias: {
       // '@': path.resolve(__dirname, './src'),
       // '@assets': path.resolve(__dirname, './src/assets'),
+      "@features": path.resolve(__dirname, "./src/features"),
       "@shared": path.resolve(__dirname, "./src/shared"),
     },
   },
