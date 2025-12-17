d9f22cee2d72630239a6688460706fa3a0dbc402
StarchenkoDmitry <StarchenkoDmitryPavlovich@gmail.com>
2025-12-17 17:21:59 +0300

    feat(frontend): add main-header

---
+++ react-frontend/src/shared/ui/headers/main-header.tsx
@@ -0,0 +1,17 @@
+export function MainHeader() {
+  return (
+    <header className="bg-white">
+      <span className="text-2xl font-mono">
+        <a href="/">Movement</a>
+      </span>
+      <span>Kek</span>
+      <nav className="inline-flex *:text-sky-500">
+        <a href="/" className="">Home</a>
+        <a href="/auth">Auth</a>
+        <a href="/auth">Me</a>
+      </nav>
+      <span>Kek2</span>
+      <h3>H3</h3>
+    </header>
+  );
+}
