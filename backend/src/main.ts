3ff13c9e084681ef92e39d340966bfa21b56d7aa
StarchenkoDmitry <StarchenkoDmitryPavlovich@gmail.com>
2025-12-20 14:31:03 +0300

    swagger

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,15 +1,23 @@
 import 'dotenv/config';
 import 'reflect-metadata';
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
 import { ValidationPipe, VersioningType } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { AppModule } from './app.module';
 import cookieParser from 'cookie-parser';
+import { SESSION_COOKIE_NAME } from './modules/auth/constants/session.constant';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     cors: {
       credentials: true,
       origin: true,
+      // allowedHeaders: 'Origin, X-Requested-With, Content-Type, Accept',
+      // allowedHeaders: '*',
+      // methods: '*',
+      // exposedHeaders: 'Set-Cookie',
+      // methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+      // origin: "*",
     },
   });
   app.use(cookieParser());
@@ -25,6 +33,19 @@ async function bootstrap() {
       transform: true,
     }),
   );
+
+  const config = new DocumentBuilder()
+    .setTitle('Cats example')
+    .setDescription('The cats API description')
+    .setVersion('1.0')
+    .addTag('cats')
+    .addCookieAuth(SESSION_COOKIE_NAME)
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory, {
+    yamlDocumentUrl: 'api/api.yaml',
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
