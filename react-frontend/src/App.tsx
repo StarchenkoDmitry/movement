f7348fb40827272943a5c7f5f491c4a444f034e0
StarchenkoDmitry <StarchenkoDmitryPavlovich@gmail.com>
2025-12-21 20:26:19 +0300

    setup

--- react-frontend/src/App.tsx
+++ react-frontend/src/App.tsx
@@ -5,23 +5,31 @@ import { AuthPage, OAuthCallbackPage } from "@pages/auth";
 import { HomePage } from "@pages/home";
 import { MePage } from "@pages/me";
 import { useApi } from "@shared/api/my-api";
+import {
+  QueryClient,
+  QueryClientProvider,
+  useQuery,
+} from "@tanstack/react-query";
 
+const queryClient = new QueryClient();
 
 useApi();
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<HomePage></HomePage>} />
-        
-        <Route path="/auth" element={<AuthPage></AuthPage>} />
-        <Route path="/auth/oauth/:provider" element={<OAuthCallbackPage />} />
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage></HomePage>} />
 
-        <Route path="/me" element={<MePage></MePage>} />
-      </Routes>
-    </BrowserRouter>
+          <Route path="/auth" element={<AuthPage></AuthPage>} />
+          <Route path="/auth/oauth/:provider" element={<OAuthCallbackPage />} />
+
+          <Route path="/me" element={<MePage></MePage>} />
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
   );
 }
 
