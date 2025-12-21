74d667cc0b7ad7700fb4e5f1fd3d9c152662a74e
StarchenkoDmitry <StarchenkoDmitryPavlovich@gmail.com>
2025-12-21 21:17:55 +0300

    update ME PAGE

--- react-frontend/src/pages/me/ui/Me.page.tsx
+++ react-frontend/src/pages/me/ui/Me.page.tsx
@@ -2,76 +2,28 @@ import { MainHeader } from "@shared/ui/headers";
 import { useQuery } from "@tanstack/react-query";
 import { useEffect, useState } from "react";
 import type { User } from "../../../orval/movement.schemas";
-import { getUserControllerMeV1QueryKey, userControllerMeV1 } from "../../../orval/user";
+import {
+  getUserControllerMeV1QueryKey,
+  getUserControllerMeV1QueryOptions,
+  userControllerMeV1,
+  userControllerUpdateV1,
+  useUserControllerMeV1,
+} from "../../../orval/user";
 
 export function MePage() {
   // const [isLoading, setIsLoading] = useState(true);
   // const [user, setUser] = useState<any>(null);
 
-  const {
-    isLoading,
-    data: user,
-    isRefetching,
-  } = useQuery<{
-    firstName: string;
-  }>({
-    initialData: {
-      // id: "",
-      // firstName: "",
-      // email: "",
-      // createdAt: new Date(),
-      // updatedAt: new Date(),
-      // credential: {
-      //   id: "",
-      //   type: "",
-      //   email: "",
-      //   passwordHash: "",
-      //   phone: "",
-      //   provider: "",
-      //   providerId: 0,
-      //   profile: {},
-      //   access: {},
-      //   accessToken: "",
-      //   refreshToken: "",
-      //   tokenExpiry: new Date(),
-      // },
-      // sessions: [],
+  const { isLoading, data: user, isRefetching } = useUserControllerMeV1();
+  // const user = data as any;
+  console.log("data", user);
 
-      firstName: "first initial data",
-    },
-    // queryKey: getUserControllerMeV1QueryKey(),
-    queryKey: ["me"],
-    queryFn: async () => {
-      // await new Promise((resolve) => setTimeout(resolve, 1000));
-      // console.log("fetch me"); 
-      return {
-        firstName: `first test ${Math.floor(Math.random() * 100000)}`,
-      };
-      // const user = await getUser();
-      // return {
-      //   firstName: user.,
-      // };save
-    },
-  });
-
-  // useEffect(() => {
-  //   fetch("http://localhost:3000/api/v1/user/me", {
-  //     credentials: "include",
-  //   })
-  //     .then((res) => {
-  //       if (res.ok) {
-  //         return res.json();
-  //       }
-  //       return null;
-  //     })
-  //     .then((data) => {
-  //       console.log("user", data);
-  //       setUser(data);
-  //     })
-  //     .finally(() => {
-  //       setIsLoading(false);
-  //     });
-  // }, []);
+  const changeName = async () => {
+    const res = await userControllerUpdateV1({
+      firstName: `Random ${Math.floor(Math.random() * 100000)}`,
+    });
+    console.log("res", res);
+  };
 
   return (
     <div>
@@ -82,15 +34,18 @@ export function MePage() {
       <div>
         {user ? (
           <>
-            {/* <div>id: {user.id}</div> */}
+            <div>id: {user.id}</div>
             <div>firstName: {user.firstName}</div>
-            {/* <div>email: {user.email}</div> */}
-            {/* <div>createdAt: {user.createdAt}</div> */}
-            {/* <div>updatedAt: {user.updatedAt}</div> */}
+            <div>createdAt: {user.createdAt}</div>
+            <div>updatedAt: {user.updatedAt}</div>
           </>
         ) : (
           "No user"
         )}
+      </div>
+      <div>
+        <h3>Mutation</h3>
+        <button onClick={changeName}>Update name</button>
       </div>
       <footer></footer>
     </div>
